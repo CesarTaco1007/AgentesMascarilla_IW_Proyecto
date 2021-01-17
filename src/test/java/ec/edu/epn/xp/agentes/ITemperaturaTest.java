d3ae3ba6d9a89ccf38ad15402f629d9ee9dad01a
ctaco <cesar.taco@epn.edu.ec>
2021-01-17 00:51:56 -0500

    Modify Mockito test unit in ITemperaturaTest class.

--- src/test/java/ec/edu/epn/xp/agentes/ITemperaturaTest.java
+++ src/test/java/ec/edu/epn/xp/agentes/ITemperaturaTest.java
@@ -9,6 +9,7 @@ public class ITemperaturaTest {
 
     @Test
     public void given_two_agents_when_tempOK_then_ok() {
+        System.out.println("Mockito Test 1");
         ITemperatura t = Mockito.mock(ITemperatura.class);
         Mockito.when(t.tempOK()).thenReturn(1);
         assertEquals(1, t.tempOK());
@@ -16,6 +17,7 @@ public class ITemperaturaTest {
 
     @Test
     public void given_two_agents_when_tempF_then_ok() {
+        System.out.println("Mockito Test 2");
         ITemperatura t = Mockito.mock(ITemperatura.class);
         Mockito.when(t.tempF()).thenReturn(0);
         assertEquals(0, t.tempOK());
